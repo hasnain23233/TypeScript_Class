4e50949e24d4e4833759334220b764cc30339693
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-06-10 16:39:00 +0500

    updatng the class10, We will learn the Tuple in typescript

---
+++ class10/app.js
@@ -0,0 +1,5 @@
+// This is class we will learn the Tuple data type in Typescript
+var TupleArray = [1, 'hasnain', true, 39];
+console.log(TupleArray);
+console.log(typeof (TupleArray));
+console.log(TupleArray[2]);
---
+++ class10/app.ts
@@ -0,0 +1,6 @@
+// This is class we will learn the Tuple data type in Typescript
+let TupleArray: [number, string, boolean, number] = [1, 'hasnain', true, 39]
+console.log(TupleArray)
+console.log(typeof (TupleArray))
+
+console.log(TupleArray[2])
