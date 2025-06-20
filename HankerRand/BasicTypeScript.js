dc8713b506daa4dad2ef5fd80ad86ecfcba38686
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-06-20 22:13:46 +0500

    In this file we practice hacker rank basic question 1 to 6

--- HankerRand/BasicTypeScript.js
+++ HankerRand/BasicTypeScript.js
@@ -1,6 +1,87 @@
 ///In this file we practice the hacker rand typesc
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Question1 = Data Types - Use multiple data types for variables. One can ask the compiler to ignore the type to take advantage of dynamic types. TS has support for ‘any’ type, primitive data types like number, boolean, string, void, null, undefined, enum.
 var StringData = 345;
 console.log(StringData);
 var useAny = 934;
 console.log(useAny);
+///Question2 = Variables Declaration - Use static type checking and TS has the same syntax as Javascript for defining variables.
+var firstVar = "Hasnain";
+console.log(firstVar);
+var secondVar = 32;
+console.log(secondVar);
+var thirdVar = true;
+console.log(thirdVar);
+var fourthVar;
+console.log(fourthVar);
+//Question3 = Array and Tuples - Use arrays of either specific or generic types. Use tuples which are arrays in which we can define the data type of each element in the array.
+var numberArray = [23, 43940, 4930];
+console.log(numberArray);
+var TupleArray2 = ['hasnain', 923, true];
+console.log(TupleArray2);
+// Function with default and rest parameters
+function addDetails(name, number) {
+    if (number === void 0) { number = 0; }
+    return {
+        name: name,
+        numberData: number
+    };
+}
+console.log(addDetails("Asifa"));
+console.log(addDetails("Asifa", 33));
+var sayHello = function (name) {
+    if (name === void 0) { name = "Guest"; }
+    console.log("Hello, " + name);
+};
+sayHello();
+sayHello("Hasnain");
+//Question5 = Class - Use of object-oriented class-based approach to creating objects.
+var AddingClass = /** @class */ (function () {
+    function AddingClass(name, email) {
+        this.email = email;
+        this.studNAme = name;
+    }
+    AddingClass.prototype.adding = function () {
+        console.log(this.studNAme, this.email);
+    };
+    return AddingClass;
+}());
+var sudent = new AddingClass('Rohail', 'RohailKhan@gmail.com');
+sudent.adding();
+//Question6 = Inheritance - Use of Inheritance to receive the features, interactions, and behaviors of a class by another class.
+var studentInfo = /** @class */ (function () {
+    function studentInfo(stu, fatherName) {
+        this.stuName = stu;
+        this.fName = fatherName;
+    }
+    return studentInfo;
+}());
+var moreDetail = /** @class */ (function (_super) {
+    __extends(moreDetail, _super);
+    function moreDetail(student, fatherName, address, city) {
+        var _this = _super.call(this, student, fatherName) || this;
+        _this.address = address;
+        _this.city = city;
+        return _this;
+    }
+    moreDetail.prototype.showingData = function () {
+        console.log([this.stuName, this.fName, this.address, this.city]);
+    };
+    return moreDetail;
+}(studentInfo));
+var sudent32 = new moreDetail('wasim', 'M Ali', 'yasin', 'gilgit');
+sudent32.showingData();
--- HankerRand/BasicTypeScript.ts
+++ HankerRand/BasicTypeScript.ts
@@ -7,3 +7,94 @@ console.log(StringData)
 
 let useAny: any = 934
 console.log(useAny)
+
+///Question2 = Variables Declaration - Use static type checking and TS has the same syntax as Javascript for defining variables.
+
+let firstVar: string = "Hasnain"
+console.log(firstVar)
+
+let secondVar: number = 32
+console.log(secondVar)
+
+let thirdVar: boolean = true
+console.log(thirdVar)
+
+let fourthVar: undefined;
+console.log(fourthVar)
+
+//Question3 = Array and Tuples - Use arrays of either specific or generic types. Use tuples which are arrays in which we can define the data type of each element in the array.
+let numberArray: number[] = [23, 43940, 4930]
+console.log(numberArray)
+let TupleArray2: [string, number, boolean] = ['hasnain', 923, true]
+console.log(TupleArray2)
+
+//Queston4 = Functions - Use functions for performing a set of actions. Create functions with typed parameters, return type annotations, function overloads, default parameter values, rest parameters. Create named functions and anonymous functions.
+type DataValues = {
+    name: string;
+    numberData: number;
+}
+
+// Function overloads
+function addDetails(name: string): DataValues;
+function addDetails(name: string, number: number): DataValues;
+
+// Function with default and rest parameters
+function addDetails(name: string, number: number = 0): DataValues {
+    return {
+        name: name,
+        numberData: number
+    };
+}
+
+console.log(addDetails("Asifa"));         
+console.log(addDetails("Asifa", 33));     
+
+const sayHello = function(name: string = "Guest") {
+    console.log("Hello, " + name);
+};
+
+sayHello(); 
+sayHello("Hasnain"); 
+
+//Question5 = Class - Use of object-oriented class-based approach to creating objects.
+class AddingClass{
+    studNAme: string
+    email: string
+    constructor(name: string, email: string) {
+        this.email = email
+        this.studNAme = name
+    }
+
+    adding(): void{
+        console.log(this.studNAme , this.email)
+    }
+}
+let sudent = new AddingClass('Rohail', 'RohailKhan@gmail.com')
+sudent.adding() 
+
+//Question6 = Inheritance - Use of Inheritance to receive the features, interactions, and behaviors of a class by another class.
+
+class studentInfo {
+    stuName: string
+    fName: string
+    constructor(stu:string ,  fatherName:string) {
+        this.stuName = stu
+        this.fName = fatherName
+    }
+}
+class moreDetail extends studentInfo {
+    address: string
+    city: string
+    constructor(student:string, fatherName:string, address:string, city:string) {
+        super(student, fatherName)
+        this.address = address
+        this.city = city
+    }
+
+    showingData ():void {
+        console.log([this.stuName , this.fName , this.address , this.city])
+    }
+}
+
+let sudent32 = new moreDetail('wasim', 'M Ali', 'yasin', 'gilgit')
+sudent32.showingData()
