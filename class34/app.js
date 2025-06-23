6235b8862c6a3722bd4499befb8510b3c32a30b3
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-06-23 23:34:10 +0500

    In this class we learn the the utility types and how to use it in the project

--- class34/app.js
+++ class34/app.js
@@ -1,23 +1,44 @@
 ///In this class we learn the the utility types and how to use it in the project
+//using Partial
 var sudent1 = {
     name: 'hasnain',
     email: 'hasnainkhushi@gmail.com'
 };
 console.log(sudent1);
+//using Required
 var sudent2 = {
     name: 'wasim',
     email: 'wasim@gmail.com',
     no: 3332392384
 };
 console.log(sudent2);
+//using ReadOnly
 var student3 = {
     name: 'Rahail',
     email: 'rohail@gmail.com',
     no: 3332392384
 };
 console.log(student3);
+//using Pick
 var student4 = {
     name: 'tofail',
     email: 'tofail@gmail.com'
 };
 console.log(student4);
+//using omit
+var student5 = {
+    name: 'Ehtisham',
+    no: 49
+};
+console.log(student5);
+var student6 = 'active';
+console.log(student6);
+var student7 = 'Hasnain';
+console.log(student7);
+var student8 = {
+    admin: 1,
+    user: 0
+};
+console.log(student8.admin);
+var student9 = 'web developer';
+console.log(student9);
--- class34/app.ts
+++ class34/app.ts
@@ -6,12 +6,15 @@ interface userData{
     no: number
 }
 
+//using Partial
 let sudent1: Partial<userData> = {
     name: 'hasnain',
     email: 'hasnainkhushi@gmail.com'
 }
 console.log(sudent1)
 
+
+//using Required
 let sudent2: Required<userData> = {
     name: 'wasim',
     email: 'wasim@gmail.com',
@@ -19,6 +22,7 @@ let sudent2: Required<userData> = {
 }
 console.log(sudent2)
 
+//using ReadOnly
 let student3: Readonly<userData> = {
     name: 'Rahail',
     email: 'rohail@gmail.com',
@@ -26,8 +30,40 @@ let student3: Readonly<userData> = {
 }
 console.log(student3)
 
+//using Pick
 let student4: Pick<userData, 'name' | 'email'> = {
     name: 'tofail',
     email: 'tofail@gmail.com'
 }
 console.log(student4)
+
+//using omit
+let student5: Omit<userData, 'email'> = {
+    name: 'Ehtisham',
+    no : 49
+}
+console.log(student5)
+
+//using exclude
+type Status = 'active' | 'offline' | 'panding'
+type checkStatus = Exclude<Status, 'offline' | 'panding'>
+const student6: checkStatus = 'active'
+console.log(student6)
+
+//using nonNullable 
+type userDataStud = string | null | undefined | number
+let student7: NonNullable<userDataStud> = 'Hasnain'
+console.log(student7)
+
+//using Record 
+type control = 'admin' | 'user'
+const student8: Record<control, number> = {
+    admin: 1, 
+    user: 0
+}
+console.log(student8.admin)
+
+//using Extract 
+type job = 'software' | 'web developer' | 'cleaner'
+const student9: Extract<job, 'software' | 'web developer'> = 'web developer'
+console.log(student9)
