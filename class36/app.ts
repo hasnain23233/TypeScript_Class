3805140e169263f2215aa6ba14878de7f2e2114c
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-06-24 18:23:58 +0500

    in this class we learn override funtion with decortor in typescript

---
+++ class36/app.ts
@@ -0,0 +1,16 @@
+function updateSportData(target: any, descriptor: PropertyDescriptor) {
+    descriptor.value = function asif(x: number, y: number) {
+        let output = x + y;
+        return `${x} + ${y} = ${output}`;
+    };
+}
+
+class sportData {
+    @updateSportData
+    asif(x: number, y: number): string {
+        return `${x + y}`;
+    }
+}
+
+let sportman = new sportData();
+console.log(sportman.asif(3, 4));
